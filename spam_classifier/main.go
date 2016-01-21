64f5e2ae66c693c898462ec087cec6d92dc99888
vroomwaddle <vroomwaddle@users.noreply.github.com>
2016-01-21 00:53:56 -0800

    clear model before all the print messages from training

--- spam_classifier/main.go
+++ spam_classifier/main.go
@@ -5,12 +5,12 @@ import (
 )
 
 func main() {
-	corpus := EnronSpamCorpus(1, 2, 3, 4, 5, 6)
 	classifier, _ := SpamClassifierFromRedisUrl("127.0.0.1:6379")
 	fmt.Println("Clearing model...")
 	classifier.dist.ResetCounts()
 	fmt.Println("Done.")
 	fmt.Println("Training...")
+	corpus := EnronSpamCorpus(1, 2, 3, 4, 5, 6)
 	classifier.StreamTrain(corpus)
 	fmt.Println("Done.")
 	fmt.Printf("Listening...")
